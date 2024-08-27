67aef8ceff28bf82ad38c3483fefe8e43381f17b
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-27 12:53:05 -0400

    Create SimpleInventory.php

---
+++ src/Resources/contao/library/Bcs/Model/SimpleInventory.php
@@ -0,0 +1,17 @@
+<?php
+ 
+namespace Bcs\Model;
+
+use Contao\Model;
+
+
+class SimpleInventory extends Model
+{
+	
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_simple_inventory';
+    
+}
