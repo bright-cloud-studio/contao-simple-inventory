dafe80400a98c8b6450dda66af149ffb6e2cbd43
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-27 12:54:17 -0400

    Create tl_simple_inventory.php

---
+++ src/Resources/contao/languages/en/tl_simple_inventory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** Legends */
+$GLOBALS['TL_LANG']['tl_simple_inventory']['product_legend'] 	= 'Product Details';
+$GLOBALS['TL_LANG']['tl_simple_inventory']['publish_legend'] 	= 'Publish';
+
+/** Fields */
+$GLOBALS['TL_LANG']['tl_simple_inventory']['alias'] 			           = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_simple_inventory']['product_name'] 			   	= array('Product Name', 'The name of the product');
+$GLOBALS['TL_LANG']['tl_simple_inventory']['product_inventory'] 		= array('Product Inventory', 'The inventory total');
+$GLOBALS['TL_LANG']['tl_simple_inventory']['published'] 		        = array('Published', 'Save this product\'s record.');
+ 
+ /** Buttons */
+$GLOBALS['TL_LANG']['tl_simple_inventory']['new']   		 	= array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_simple_inventory']['show']  		 	= array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_simple_inventory']['edit']  		 	= array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_simple_inventory']['copy']  		 	= array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_simple_inventory']['delete'] 			= array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_simple_inventory']['toggle'] 			= array('Toggle record', 'Toggle record ID %s');
