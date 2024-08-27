6c937f06800a1ee1e13b107f6e905a733b887bd1
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-27 12:38:21 -0400

    Create BcsSimpleInventoryBundle.php

---
+++ src/BcsSimpleInventoryBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Simple Inventory
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-simple-inventory
+ */
+
+namespace Bcs\SimpleInventoryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsSimpleInventoryBundle extends Bundle
+{
+}
